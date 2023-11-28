23c96f053f4cd02faa6dda51ee90f3d6bef983bd
kimse2771 <90876202+kimse2771@users.noreply.github.com>
2023-11-28 16:32:18 +0900

    [level unrated] Title: 공 던지기, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
@@ -1,16 +1,5 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
-        int num=0;
-        for(int i =0;i<k-1;i++){
-            if(num+2>=numbers.length){
-                num=num+2-numbers.length;
-            }else{
-                num+=2;
-            }
-            answer=numbers[num];
-        }
-        
-        return answer;
+        return (k-1)*2 % numbers.length+1;
     }
 }
