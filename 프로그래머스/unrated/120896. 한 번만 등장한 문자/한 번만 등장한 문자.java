1bfe847823278ae93555c3242e95f7aa8aedb087
kimse2771 <90876202+kimse2771@users.noreply.github.com>
2023-12-01 11:41:24 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 1.60 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+        Arrays.sort(str);
+        int cnt=0;
+        for(int i=0;i<str.length;i++){
+            cnt=0;
+            for(int j=0;j<str.length;j++){
+                if(str[i].equals(str[j])){
+                    cnt++;
+                }
+                
+            }
+            if(cnt==1){
+                answer+=str[i];
+            }
+            
+        }
+        return answer;
+    }
+}
