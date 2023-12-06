06e57c7e9a8acb540a4938c6895bcd971feea8fb
kimse2771 <90876202+kimse2771@users.noreply.github.com>
2023-12-06 15:49:10 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.56 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        if(numbers[0]<0&&numbers[1]<0){
+            if(numbers[0]*numbers[1]>numbers[numbers.length-2]*numbers[numbers.length-1]){
+                answer = numbers[0]*numbers[1];
+            }else{
+               answer= numbers[numbers.length-2]*numbers[numbers.length-1];
+            }
+        }else{
+            answer = numbers[numbers.length-2]*numbers[numbers.length-1];
+        }
+        return answer;
+    }
+}
