f9e23db0659d05012bdad208922d9a92091c6d35
kimse2771 <90876202+kimse2771@users.noreply.github.com>
2024-01-05 15:28:59 +0900

    [level 1] Title: 모의고사, Time: 1.26 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,71 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        
-        
-        List<Integer> student1= new LinkedList<>(Arrays.asList(1, 2, 3,4,5));
-        List<Integer> student2= new LinkedList<>(Arrays.asList(2, 1, 2, 3, 2, 4, 2, 5));
-        List<Integer> student3= new LinkedList<>(Arrays.asList(3, 3, 1, 1, 2, 2, 4, 4, 5, 5));
-            
-        
-        int[] std1 ={1,2,3,4,5};
-        int[] std2 ={2, 1, 2, 3, 2, 4, 2, 5};
-        int[] std3={3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int temp=(answers.length/std1.length)+(answers.length%std1.length)*1;
-        
-        for(int i=0;i<temp;i++){
-            for(int s:std1){
-                student1.add(s);
-            }
-        }
-        
-        temp=(answers.length/std2.length)+(answers.length%std2.length)*1;
-        
-        for(int i=0;i<temp;i++){
-            for(int s:std2){
-                student2.add(s);
-            }
-        }
-        
-        temp=(answers.length/std3.length)+(answers.length%std3.length)*1;
-        
-        for(int i=0;i<temp;i++){
-            for(int s:std3){
-                student3.add(s);
-            }
-        }
-        List<Integer> stdAns=new ArrayList<>();
+        int[][] student = {
+            {1, 2, 3,4,5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
         int[] stdCnt=new int[3];
         
-        for(int i =0;i<answers.length;i++){
-            if(answers[i]==student1.get(i)){
-                stdCnt[0]++;
-            }
-            if(answers[i]==student2.get(i)){
-                stdCnt[1]++;
-            }
-            if(answers[i]==student3.get(i)){
-                stdCnt[2]++;
+        for(int i=0;i<student.length;i++){
+            for(int j=0;j<answers.length;j++){
+                if(student[i][j%student[i].length]==answers[j]){
+                    stdCnt[i]++;
+                }
             }
         }
         
-        int max=0;
+        int max=Math.max(stdCnt[0],Math.max(stdCnt[1],stdCnt[2]));
         
-        for(int s : stdCnt){
-            if(max<s){
-                max=s;
-            }
-        }
+        List<Integer> list=new ArrayList<>();
         
         for(int i=0;i<3;i++){
-            if(max==stdCnt[i]){
-                stdAns.add(i+1);
+            if(stdCnt[i]==max){
+                list.add(i+1);
             }
         }
         
-        int[] answer = new int[stdAns.size()];
-        for(int i=0;i<stdAns.size();i++){
-            answer[i]=stdAns.get(i);
+        int[] answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i]=list.get(i);
         }
         
         
