26cf085174abdaa593fd4afd22fa7156027b554b
kimse2771 <90876202+kimse2771@users.noreply.github.com>
2024-01-18 14:30:49 +0900

    [Bronze II] Title: 커트라인, Time: 272 ms, Memory: 20392 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner s= new Scanner(System.in);
+        int n = s.nextInt();
+        int k = s.nextInt();
+        Integer[] arr = new Integer[n];       
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+           Arrays.sort(arr,Collections.reverseOrder());
+        
+        System.out.println(arr[k-1]);
+        
+    }
+        
+}
