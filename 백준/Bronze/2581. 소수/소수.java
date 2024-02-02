ba4a20cd6a79adeede01a77d375314bc1cf143fd
kimse2771 <90876202+kimse2771@users.noreply.github.com>
2024-02-02 15:32:01 +0900

    [Bronze II] Title: 소수, Time: 152 ms, Memory: 16044 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,41 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int M = Integer.parseInt(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-		int min = N;
-		int sum = 0;
-		
-		for(int i = M; i <= N; i++) {
-			//소수인지 판별할 변수
-			boolean check = true;
-			//1이 올 수 있지만 1은 소수가 아니기에 chech에 false 저장.
-			if(i == 1) check = false;
-			
-			for(int j = 2; j < i; j++) {
-				//나누어 떨어진다면 check에 false 저장하고 반복 종료
-				if(i % j == 0) {
-					check = false;
-					break;
-				}
-			}
-			
-			//check가 true라면 실수이니 합하고, 최솟값 저장.
-			if(check) {
-				if(min > i) min = i;
-				sum += i;
-			}
-		}
-		if(sum == 0) {
-			System.out.println(-1);
-		}else {
-			System.out.println(sum + "\n" + min);
-		}
-	}
 
+class Main{
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        boolean check = false;
+        int sum=0;
+        int min =M;
+        
+        
+        for(int i=N;i<=M;i++){
+        	check = true;
+            
+            if(i==1)check=false;
+            for(int j=2;j<=Math.sqrt(i);j++){	 
+              if(i%j==0){
+                  check=false;
+                  break;
+              }
+
+            }
+            if(check){
+                if(min>i){
+                    min=i;
+                }
+                sum+=i;
+            }
+        }
+        
+       if(!check&&sum==0){
+        	System.out.println(-1);
+   		 
+        }else{
+            System.out.println(sum+"\n"+min);
+        }
+        
+    }
+    
 }
