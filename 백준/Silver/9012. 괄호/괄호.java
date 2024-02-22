635995151d6978872f7cac3d11782b6c42c9537c
kimse2771 <90876202+kimse2771@users.noreply.github.com>
2024-02-22 17:45:23 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,5 +1,7 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
 
 class Main{
     public static void main(String[] args) throws Exception{
