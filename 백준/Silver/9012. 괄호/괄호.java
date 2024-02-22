f9829e1fa2f369e21c179519c8b071c96b49d267
kimse2771 <90876202+kimse2771@users.noreply.github.com>
2024-02-22 17:49:56 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Stack;
 
 class Main{
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb= new StringBuilder();
         
